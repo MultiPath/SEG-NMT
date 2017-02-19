ba30d7f9bee88701c99d0223bd757d2956829264
Jiatao Gu <thomagram@gmail.com>
2017-02-19 04:02:23 -0500

    clean the code, perform the baseline model.

--- dl4mt/train_nmt.py
+++ dl4mt/train_nmt.py
@@ -1,50 +1,46 @@
-import numpy
-import os
-
-import numpy
-import os
-
 from nmt import train
+from pprint import pprint
 
-def main(job_id, params):
-    print params
-    validerr = train(saveto=params['model'][0],
-                     reload_=params['reload'][0],
-                     dim_word=params['dim_word'][0],
-                     dim=params['dim'][0],
-                     n_words=params['n-words'][0],
-                     n_words_src=params['n-words'][0],
-                     decay_c=params['decay-c'][0],
-                     clip_c=params['clip-c'][0],
-                     lrate=params['learning-rate'][0],
-                     optimizer=params['optimizer'][0],
-                     patience=1000,
-                     maxlen=50,
-                     batch_size=32,
-                     valid_batch_size=32,
-                     validFreq=100,
-                     dispFreq=10,
-                     saveFreq=100,
-                     sampleFreq=100,
-                     datasets=['../data/hal/train/tok/en',
-                               '../data/hal/train/tok/fr'],
-                     valid_datasets=['../data/hal/dev/tok/en',
-                                     '../data/hal/dev/tok/fr'],
-                     dictionaries=['../data/hal/train/tok/en.pkl',
-                                   '../data/hal/train/tok/fr.pkl'],
-                     use_dropout=params['use-dropout'][0],
-                     overwrite=False)
-    return validerr
+def setup():
+    home   = '/misc/kcgscratch1/ChoGroup/thoma_exp/memory/TMNMT'
+    config = {
+        'saveto': home + '/.model/baseline_fren.npz',
+        'datasets': [home + '/.dataset/train.fr.tok.shuf',
+                     home + '/.dataset/train.en.tok.shuf'],
+        'valid_datasets': [home + '/.dataset/devset.fr.tok',
+                           home + '/.dataset/devset.en.tok'],
+        'dictionaries': [home + '/.dataset/train.fr.tok.pkl',
+                         home + '/.dataset/train.en.tok.pkl'],
 
-if __name__ == '__main__':
-    main(0, {
-        'model': ['model_hal.npz'],
-        'dim_word': [512],
-        'dim': [1024],
-        'n-words': [30000],
-        'optimizer': ['adadelta'],
-        'decay-c': [0.],
-        'clip-c': [1.],
-        'use-dropout': [False],
-        'learning-rate': [0.0001],
-        'reload': [True]})
+        'dim_word': 512,
+        'dim': 1024,
+        'n_words':     20000,
+        'n_words_src': 20000,
+
+        'optimizer': 'adam',
+        'decay_c': 0.,
+        'clip_c': 1.,
+        'use_dropout': False,
+
+        'lrate': 0.00002,
+
+        'patience':1000,
+        'maxlen': 50,
+        'batch_size':32,
+        'valid_batch_size':32,
+        'validFreq':100,
+        'dispFreq':10,
+        'saveFreq':100,
+        'sampleFreq':100,
+
+        'overwrite': False,
+        'reload_': True}
+    return config
+
+
+config = setup()
+pprint(config)
+
+validerr = train(**config)
+
+print 'done'
